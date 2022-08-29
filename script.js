8c6c4bc4d0d5418f90b1fcff9bb37fb62a549628
murosaka <murosaka@gmail.com>
2022-08-29 16:11:30 +0300

    add prompt and redraw

--- script.js
+++ script.js
@@ -5,21 +5,31 @@ const container = document.querySelector('#container');
 //cell.classList.add('cell');
 //row.textContent = 'row';
 //cell.textContent = 'cell';
-for (let i = 0; i < 16; i++) {
-    let row = document.createElement('div');
-    //row.innerText = 'row'+(i+1);
-    container.appendChild(row).className = 'row';
-    for (let j = 0; j < 16; j++) {
-        let cell = document.createElement('div');
-        //cell.innerText = '|'+(j+1)+'|';
-        row.appendChild(cell).className = 'cell';
+let row;
+let cell;
+let num; 
+document.getElementById('grid').addEventListener("click", drawGrid);
+
+function drawGrid (num) {
+    num = prompt("Enter number", 16);
+    let rows = document.querySelectorAll(".row");
+    rows.forEach(row => {container.removeChild(row);}); 
+    for (let i = 0; i < num; i++) {
+        row = document.createElement('div');
+        //row.innerText = 'row'+(i+1);
+        container.appendChild(row).className = 'row';
+        for (let j = 0; j < num; j++) {
+            cell = document.createElement('div');
+            //cell.innerText = '|'+(j+1)+'|';
+            row.appendChild(cell).className = 'cell';
+        }
     }
+    let elements = document.querySelectorAll(".cell");
+    elements.forEach(element => {
+        element.addEventListener("mouseover", 
+        () => element.setAttribute('style','background-color: black')); 
+        element.addEventListener("click", 
+        () => element.setAttribute('style','background-color: '));   
+    });
 }
 
-let elements = document.querySelectorAll(".cell");
-elements.forEach(element => {
-    element.addEventListener("mouseover", 
-    () => element.setAttribute('style','background-color: black')); 
-    element.addEventListener("click", 
-    () => element.setAttribute('style','background-color: '));   
-});
