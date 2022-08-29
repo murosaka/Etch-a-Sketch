3cb2561b1c9e0a1620dfb86c2ba0f40a306e5d25
murosaka <murosaka@gmail.com>
2022-08-29 16:16:11 +0300

    some code cleaning

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
 const container = document.querySelector('#container');
-//const cell = document.createElement('div');
-//const cell = document.createElement('div');
-//row.classList.add('row');
-//cell.classList.add('cell');
-//row.textContent = 'row';
-//cell.textContent = 'cell';
 let row;
 let cell;
 let num; 
@@ -16,11 +10,9 @@ function drawGrid (num) {
     rows.forEach(row => {container.removeChild(row);}); 
     for (let i = 0; i < num; i++) {
         row = document.createElement('div');
-        //row.innerText = 'row'+(i+1);
         container.appendChild(row).className = 'row';
         for (let j = 0; j < num; j++) {
             cell = document.createElement('div');
-            //cell.innerText = '|'+(j+1)+'|';
             row.appendChild(cell).className = 'cell';
         }
     }
